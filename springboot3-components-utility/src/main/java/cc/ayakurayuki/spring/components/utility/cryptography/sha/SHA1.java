5151f14de09c3004439d1f484494cb2260049804
Ayakura Yuki <YukiAyakura@outlook.com>
2025-06-05 18:23:50 +0800

    + SHA

---
+++ springboot3-components-utility/src/main/java/cc/ayakurayuki/spring/components/utility/cryptography/sha/SHA1.java
@@ -0,0 +1,65 @@
+package cc.ayakurayuki.spring.components.utility.cryptography.sha;
+
+import cc.ayakurayuki.spring.components.utility.cryptography.DigestAlgorithm;
+import cc.ayakurayuki.spring.components.utility.cryptography.digest.Digests;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.security.NoSuchAlgorithmException;
+
+/**
+ * @author Ayakura Yuki
+ */
+public final class SHA1 {
+
+  /**
+   * 对输入的 bytes 进行 sha1 散列
+   */
+  public static byte[] digest(byte[] input) throws NoSuchAlgorithmException {
+    return Digests.digest(input, DigestAlgorithm.SHA1, null);
+  }
+
+  /**
+   * 对输入的 bytes 进行 sha1 散列，带 salt 达到更高的安全性
+   */
+  public static byte[] digest(byte[] input, byte[] salt) throws NoSuchAlgorithmException {
+    return Digests.digest(input, DigestAlgorithm.SHA1, salt);
+  }
+
+  /**
+   * 对输入的字符串进行 sha1 散列
+   */
+  public static byte[] digest(String input) throws NoSuchAlgorithmException {
+    return Digests.digest(input.getBytes(StandardCharsets.UTF_8), DigestAlgorithm.SHA1, null);
+  }
+
+  /**
+   * 对输入的字符串进行 sha1 散列
+   */
+  public static byte[] digest(String input, Charset charset) throws NoSuchAlgorithmException {
+    return Digests.digest(input.getBytes(charset), DigestAlgorithm.SHA1, null);
+  }
+
+  /**
+   * 对输入的字符串进行 sha1 散列，带 salt 达到更高的安全性
+   */
+  public static byte[] digest(String input, byte[] salt) throws NoSuchAlgorithmException {
+    return Digests.digest(input.getBytes(StandardCharsets.UTF_8), DigestAlgorithm.SHA1, salt);
+  }
+
+  /**
+   * 对输入的字符串进行 sha1 散列，带 salt 达到更高的安全性
+   */
+  public static byte[] digest(String input, Charset charset, byte[] salt) throws NoSuchAlgorithmException {
+    return Digests.digest(input.getBytes(charset), DigestAlgorithm.SHA1, salt);
+  }
+
+  /**
+   * 对文件进行 sha1 散列
+   */
+  public static byte[] digest(InputStream inputStream) throws NoSuchAlgorithmException, IOException {
+    return Digests.digest(inputStream, DigestAlgorithm.SHA1);
+  }
+
+}
